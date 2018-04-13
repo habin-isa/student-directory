9c4a740d02a5dcabd521d6b8de3bd7c961a232ec
Habin <habin@hotmail.co.uk>
2018-04-14 00:04:26 +0100

    added try_load_students method

--- directory.rb
+++ directory.rb
@@ -74,6 +74,18 @@ def load_students(filename = "students.csv")
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
